d9542d561bee96935e158ffbf4c57cad30a38a69
kohei.suzuki <3173010@stu.hus.ac.jp>
2019-03-14 05:42:22 +0000

    make image_timeline_system

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -2,10 +2,10 @@ class Timeline < ApplicationRecord
   belongs_to :user
   
   validates :content, presence:true, length:{maximum:255}
-  validates :image, length:{maximum:255}
-  validates :video, length:{maximum:255}
-  
+
   has_many :goods, dependent: :destroy
   has_many :liked_users, through: :goods, source: :user
+  
+  mount_uploader :image, ImageUploader
 
 end
